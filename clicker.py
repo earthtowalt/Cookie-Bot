eaad508019087ea4f4f005e939d4d92705019be2
earthtowalt <jackwalton00@aol.com>
2019-09-21 17:47:40 -0500

    committing changes

--- clicker.py
+++ clicker.py
@@ -1,87 +1,98 @@
 """
 An extremely simple bot that plays cookie clicker on an assumed 1080p resolution monitor with cookie clicker open in a full web browser.
 To use, go to this page http://orteil.dashnet.org/cookieclicker/ in full screen and start the program.
-
 Controls:
 Z: Clicks the cookie.
 X: Buys new buildings.
 C: Buys Upgrades
 ESC: Closes out of the program.
-
 These keys can be pressed from any location on your OS and are recognized using the same tech that is used for key loggers.
 """
 
 import pyautogui
 import time
-import sys
+import sys, os
 from pynput import keyboard
 import _thread
 
 flags = {}
-flags["buy upgrades"] = False
+flags["buy things"] = False
 flags["click cookie"] = False
-flags["buy buildings"] = False
+
 
 def click_cookie():
     global flags
     while(True):
         if flags['click cookie']:
-            pyautogui.click(264, 468, 2)
-            time.sleep(0.00002)
+            pyautogui.click(-1636, 157, 50)
+            time.sleep(0.1)
 
-def buy_buildings():
-    global flags
-    while(True):
-        if flags['buy buildings']:
-            pyautogui.click(1736, 354)
-            pyautogui.click(1643, 427)
-            pyautogui.click(1734, 507)
-            pyautogui.click(1750, 562)
-            pyautogui.click(1750, 630)
-            pyautogui.click(1659, 715)
-            pyautogui.click(1736, 808)
-            pyautogui.click(1716, 853)
-            pyautogui.click(1752, 946)
-            pyautogui.click(1701, 999)
-            time.sleep(5)
-def buy_upgrades():
-    while(True):
-        if flags['buy upgrades']:
-            pyautogui.click(1581, 219)
-            time.sleep(5)
+def buy_things():
+	global flags
+	while(True):
+		time.sleep(3)
+		if flags['buy things']:
+			pyautogui.click(-291,-76, 4) # upgrade
+			time.sleep(1)
+			pyautogui.click(-185,572,4)
+			time.sleep(.1)
+			pyautogui.click(-185,500,4)
+			time.sleep(.1)
+			'''
+			## balanced
+			time.sleep(.1)
+			pyautogui.click(-185,410,4)
+			time.sleep(.1)
+			pyautogui.click(-185,350,4)
+			time.sleep(.1)
+			pyautogui.click(-185,285,4)
+			time.sleep(.1)
+			pyautogui.click(-185,220,4)
+			time.sleep(.1)
+			pyautogui.click(-185,150,4)
+			time.sleep(.1)
+			pyautogui.click(-185,90,4)
+			time.sleep(.1)
+			pyautogui.click(-185,30,4)
+			time.sleep(.1)
+		'''
+		time.sleep(60 * 10)
 def on_press(key):
+	pass 
+	'''
     try:
         print('{0}'.format(
             key.char))
     except AttributeError:
         print('{0}'.format(
-            key))
+            key))'''
 
 def on_release(key):
-    global flags
+	global flags
 
-    try:
-        if key.char == 'z':
-            flags['click cookie'] = not flags['click cookie']
-        if key.char == 'x':
-            flags['buy buildings'] = not flags['buy buildings']
-        if key.char == 'c':
-            flags['buy upgrades'] = not flags['buy upgrades']
-    except AttributeError:
-        pass
+	try:
+		if key.char == 'c':
+			flags['click cookie'] = not flags['click cookie']
+		if key.char == 'b':
+			flags['buy things'] = not flags['buy things']
+		if key.char == 'c' or key.char == 'b':
+			os.system('cls')
+			print('clicking cookies: ' + str(flags['click cookie']))
+			print('buying things: ' + str(flags['buy things']))
+			
+	except AttributeError:
+		pass
 
-    # Stop listener
-    if key == keyboard.Key.esc:
-        
-        sys.exit()
+	# Stop listener
+	if key == keyboard.Key.esc:
+		sys.exit()
 
 
 
 if __name__ == "__main__":
     # Spin up clicking threads.
     _thread.start_new_thread(click_cookie, ())
-    _thread.start_new_thread(buy_buildings, ())
-    _thread.start_new_thread(buy_upgrades, ())
+    _thread.start_new_thread(buy_things, ())
 
 
     # Collect events until released
---
+++ location_finder.py
@@ -0,0 +1,10 @@
+
+import pyautogui
+import time
+
+if __name__ == "__main__":
+	
+	#game_controller()
+	while True:
+		print(pyautogui.position())
+		time.sleep(0.1)
